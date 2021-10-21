550daef4fc7c6ed4e8dffd7c30a78b4d9a1441bf
Stephan benchemoul <stephanb@moveo.co.il>
2021-10-21 09:50:37 +0300

    order

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { NextpageComponent } from './pages/nextpage/nextpage.component';
+import { RestaurantsPageComponent } from './pages/restaurants-page/restaurants-page.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent, data: { animation: 'isLeft'} },
-  { path: 'next', component: NextpageComponent, data: { animation: 'isRight'} }
+  { path: 'restaurants', component: RestaurantsPageComponent, data: { animation: 'isRight'} }
 ];
 
 @NgModule({
--- src/app/services/restaurants.service.ts
+++ src/app/services/restaurants.service.ts
@@ -9,7 +9,7 @@ export class RestaurantsService {
   constructor(public iconsService: IconsService) { }
 
   get filteredChefRest() {
-    const cArr = this.restaurantsArr.chefs 
+    const cArr = this.restaurantsArr.chefs
     const filterdRests = cArr[0].chefRest
     return filterdRests
   }
@@ -62,6 +62,7 @@ export class RestaurantsService {
           }
         ]
       },
+      //epicure-app/src/assets/img/Rests/messa_2021-10-20/messa.png
       {
         restName: 'Popina',
         img: '',
@@ -75,8 +76,24 @@ export class RestaurantsService {
             price: '65'
           }
         ]
-      }
+      },
+      {
+        restName: 'Messa',
+        img: '',
+        chef: 'Aviv Moshe',
+        dishes: [
+          {
+            dishName: 'Smoked Pizza',
+            img: 'assets/img/Rests/messa_2021-10-20/messa.png',
+            description: 'Basil dough, cashew "butter", demi-glace, bison & radish',
+            logo: this.iconsService.leafIcon,
+            price: '65'
+          }
+        ]
+      },
+
     ],
+
     chefs: [
       {
         chef: 'Yossi Shitrit',
@@ -90,79 +107,4 @@ export class RestaurantsService {
       }
     ]
   }
-
-  // restaurantsArr = [
-  //   {
-  //     restArr: [
-  //       {
-  //         restName: 'Claro',
-  //         img: 'assets/img/Rests/All/claro.jpg',
-  //         chef: 'Ren Shmueli',
-  //         dishes: []
-  //       },
-  //       {
-  //         restName: 'Lumina',
-  //         img: 'assets/img/Rests/All/mizlala-gret-mullet-fillet.jpg',
-  //         chef: 'Meir Adoni',
-  //         dishes: []
-  //       },
-  //       {
-  //         restName: 'Tiger Lilly',
-  //         img: 'assets/img/Rests/All/tiger-lili.jpg',
-  //         chef: 'Ren Shmueli',
-  //         dishes: [
-  //           {
-  //             dishName: 'Pad Ki Mao',
-  //             img: 'assets/img/Rests/All/pad-ki-mao.jpg',
-  //             description: "Shrimps, Glass Noodles, Kemiri Nuts, Shallots, Lemon Grass, Magic Chili Brown Coconut",
-  //             logo: this.iconsService.chilyIcon,
-  //             price: '88'
-  //           }
-  //         ]
-  //       },
-  //       {
-  //         restName: 'Kab Kem',
-  //         img: '',
-  //         chef: '',
-  //         dishes: [
-  //           {
-  //             dishName: 'Garbanzo Frito',
-  //             img: 'assets/img/Rests/All/kab-kem.jpg',
-  //             description: "Polenta fingers, veal cheek, magic chili cured lemon cream, yellow laksa",
-  //             logo: '',
-  //             price: '98'
-  //           }
-  //         ]
-  //       },
-  //       {
-  //         restName: 'Popina',
-  //         img: '',
-  //         chef: '',
-  //         dishes: [
-  //           {
-  //             dishName: 'Smoked Pizza',
-  //             img: 'assets/img/Rests/All/popina.jpg',
-  //             description: 'Basil dough, cashew "butter", demi-glace, bison & radish',
-  //             logo: this.iconsService.leafIcon,
-  //             price: '65'
-  //           }
-  //         ]
-  //       }]
-  //   },
-  //   {
-  //     chefs: [
-  //       {
-  //         chef: 'Yossi Shitrit',
-  //         img: 'assets/img/Rests/All/yossi.jpg',
-  //         description: "Chef Yossi Shitrit has been living and breathing his culinary dreams for more than two decades, including running the kitchen in his first restaurant, the fondly-remembered Violet, located in Moshav  Udim. Shitrit's creativity and culinary  acumen born of long experience  are expressed in the every detail of each andevery dish.",
-  //         chefRest: [
-  //           { restName: 'Onza', img: 'assets/img/Rests/All/onza.jpg' },
-  //           { restName: 'Kitchen Market', img: 'assets/img/Rests/All/kitchen-market.jpg' },
-  //           { restName: 'Mashya', img: 'assets/img/Rests/All/mashya.jpg' },
-  //         ]
-  //       }
-  //     ]
-  //   }
-  // ]
-
 }
